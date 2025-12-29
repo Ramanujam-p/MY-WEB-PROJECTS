030204cce72454db03c2248361c869f94e5c106e
Ramanujam-p <pramanujam68@gmail.com>
2025-12-29 18:39:20 +0530

    Code By Rama

--- XML/TRADITIONAL WAY/LIBRARY MANAGEMENT SYSTEM/script.js
+++ XML/TRADITIONAL WAY/LIBRARY MANAGEMENT SYSTEM/script.js
@@ -1,52 +1,46 @@
-function loadxml() {
+function loadxml()
+{
+    alert("Book details loaded successfully");
     let a = new XMLHttpRequest();
-    a.onreadystatechange = () => {
-        if (a.readyState === 4 && a.status === 200) {
+    a.onreadystatechange=()=>{
+        if(a.readyState===4 && a.status===200)
+        {
             let b = a.responseXML;
-            let c = b.getElementsByTagName("books");
-            let output = `<table border="5">
+            let c = b.getElementsByTagName("book");
+            let output=`<table border="1" cell-padding="10">
             <tr>
-                <th>Title</th>
-                <th>Id</th>
-                <th>Author</th>
-                <th>Cateogry</th>
-                <th>Availability</th>
+                <th>ID</th>
+                <th>TITLE</th>
+                <th>AUTHOR</th>
+                <th>YEAR</th>
+                <th>GENRE</th>
+                <th>AVAILABLE</th>
             </tr>
             `;
-            for (let i = 0; i < c.length; i++) {
-                let v = c[i].getElementsByTagName("Title")[0].textContent;
-                let w = c[i].getElementsByTagName("Id")[0].textContent;
-                let x = c[i].getElementsByTagName("Author")[0].textContent;
-                let y = c[i].getElementsByTagName("cateogry")[0].textContent;
-                let z = c[i].getElementsByTagName("Availability")[0].textContent;
-                
-                if (z === "Yes") {
-                    output += `
-                <tr class="Book">
-                    <td>${v}</td>
-                    <td>${w}</td>
-                    <td>${x}</td>
-                    <td>${y}</td>
-                    <td>${z}</td>     
+            for(let i=0;i<c.length;i++)
+            {
+                let p = c[i].getElementsByTagName("id")[0].textContent;
+                let q = c[i].getElementsByTagName("title")[0].textContent;
+                let r = c[i].getElementsByTagName("author")[0].textContent;
+                let s = c[i].getElementsByTagName("year")[0].textContent;
+                let t = c[i].getElementsByTagName("genre")[0].textContent;
+                let u = c[i].getElementsByTagName("available")[0].textContent;
+                let col = (u==="Yes")?"green":"red";
+                output+=`
+                <tr style="background-color:${col}">
+                    <td>${p}</td>
+                    <td>${q}</td>
+                    <td>${r}</td>
+                    <td>${s}</td>
+                    <td>${t}</td>
+                    <td>${u}</td>
                 </tr>
                 `;
-                }
-                else {
-                    output += `
-                    <tr class="Book Unavailable">
-                        <td>${v}</td>
-                        <td>${w}</td>
-                        <td>${x}</td>
-                        <td>${y}</td>
-                        <td>${z}</td>
-                    </tr>
-
-                    `
-                }
             }
-            document.getElementById("output").innerHTML = output;
+            output+=`</table>`;
+            document.getElementById("output").innerHTML=output;            
         }
     }
-    a.open("GET", "library.xml", true);
+    a.open("GET","lms.xml",true);
     a.send();
 }
