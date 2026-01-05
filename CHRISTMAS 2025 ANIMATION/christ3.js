6b243f48698a80e0d4a853cb68bdacba6d71dfa7
Ramanujam-p <pramanujam68@gmail.com>
2026-01-05 22:50:27 +0530

    Code By Rama

--- CHRISTMAS 2025 ANIMATION/christ3.js
+++ CHRISTMAS 2025 ANIMATION/christ3.js
@@ -1,115 +1,244 @@
-gsap.registerPlugin(
-  MotionPathPlugin,
-  DrawSVGPlugin,
-  MorphSVGPlugin,
-  Physics2DPlugin
-);
-
 MorphSVGPlugin.convertToPath("polygon");
+var xmlns = "http://www.w3.org/2000/svg",
+  xlinkns = "http://www.w3.org/1999/xlink",
+  select = function (s) {
+    return document.querySelector(s);
+  },
+  selectAll = function (s) {
+    return document.querySelectorAll(s);
+  },
+  pContainer = select(".pContainer"),
+  mainSVG = select(".mainSVG"),
+  star = select("#star"),
+  sparkle = select(".sparkle"),
+  tree = select("#tree"),
+  showParticle = true,
+  particleColorArray = [
+    "#E8F6F8",
+    "#ACE8F8",
+    "#F6FBFE",
+    "#A2CBDC",
+    "#B74551",
+    "#5DBA72",
+    "#910B28",
+    "#910B28",
+    "#446D39"
+  ],
+  particleTypeArray = ["#star", "#circ", "#cross", "#heart"],
+  // particleTypeArray = ['#star'],
+  particlePool = [],
+  particleCount = 0,
+  numParticles = 201;
 
-const select = s => document.querySelector(s);
-const selectAll = s => document.querySelectorAll(s);
+gsap.set("svg", {
+  visibility: "visible"
+});
 
-const mainSVG = select(".mainSVG");
-const pContainer = select(".pContainer");
-const sparkle = select(".sparkle");
+gsap.set(sparkle, {
+  transformOrigin: "50% 50%",
+  y: -100
+});
 
-const particleColorArray = [
-  "#E8F6F8", "#ACE8F8", "#F6FBFE",
-  "#A2CBDC", "#B74551", "#5DBA72",
-  "#910B28", "#910B28", "#446D39"
-];
+let getSVGPoints = (path) => {
+  let arr = [];
+  var rawPath = MotionPathPlugin.getRawPath(path)[0];
+  rawPath.forEach((el, value) => {
+    let obj = {};
+    obj.x = rawPath[value * 2];
+    obj.y = rawPath[value * 2 + 1];
+    if (value % 2) {
+      arr.push(obj);
+    }
+  });
 
-const particleTypeArray = ["#star", "#circ", "#cross", "#heart"];
-const particlePool = [];
-let particleCount = 0;
-const numParticles = 200;
-let showParticle = true;
+  return arr;
+};
+let treePath = getSVGPoints(".treePath");
 
-gsap.set("svg", { visibility: "visible" });
-gsap.set(sparkle, { transformOrigin: "50% 50%", y: -100 });
+var treeBottomPath = getSVGPoints(".treeBottomPath");
 
-function getSVGPoints(path) {
-  const rawPath = MotionPathPlugin.getRawPath(path)[0];
-  const points = [];
-  for (let i = 0; i < rawPath.length; i += 2) {
-    points.push({ x: rawPath[i], y: rawPath[i + 1] });
-  }
-  return points;
-}
+//console.log(starPath.length)
+var mainTl = gsap.timeline({ delay: 0, repeat: 0 }),
+  starTl;
 
-const treePath = getSVGPoints(".treePath");
-const treeBottomPath = getSVGPoints(".treeBottomPath");
+//tl.seek(100).timeScale(1.82)
 
 function flicker(p) {
-  gsap.killTweensOf(p);
-  gsap.fromTo(p, { opacity: 1 }, {
-    opacity: Math.random(),
-    duration: 0.07,
-    repeat: -1
-  });
+  //console.log("flivker")
+  gsap.killTweensOf(p, { opacity: true });
+  gsap.fromTo(
+    p,
+    {
+      opacity: 1
+    },
+    {
+      duration: 0.07,
+      opacity: Math.random(),
+      repeat: -1
+    }
+  );
 }
 
 function createParticles() {
-  for (let i = 0; i < numParticles; i++) {
-    const p = select(particleTypeArray[i % particleTypeArray.length]).cloneNode(true);
+  //var step = numParticles/starPath.length;
+  //console.log(starPath.length)
+  var i = numParticles,
+    p,
+    particleTl,
+    step = numParticles / treePath.length,
+    pos;
+  while (--i > -1) {
+    p = select(particleTypeArray[i % particleTypeArray.length]).cloneNode(true);
     mainSVG.appendChild(p);
     p.setAttribute("fill", particleColorArray[i % particleColorArray.length]);
     p.setAttribute("class", "particle");
-    gsap.set(p, { x: -100, y: -100, transformOrigin: "50% 50%" });
     particlePool.push(p);
+    //hide them initially
+    gsap.set(p, {
+      x: -100,
+      y: -100,
+      transformOrigin: "50% 50%"
+    });
   }
 }
 
-const getScale = gsap.utils.random(0.5, 3, 0.001, true);
+var getScale = gsap.utils.random(0.5, 3, 0.001, true);
 
-function playParticle() {
-  if (!showParticle) return;
-
-  const p = particlePool[particleCount];
+function playParticle(p) {
+  if (!showParticle) {
+    return;
+  }
+  var p = particlePool[particleCount];
   gsap.set(p, {
-    x: gsap.getProperty(pContainer, "x"),
-    y: gsap.getProperty(pContainer, "y"),
+    x: gsap.getProperty(".pContainer", "x"),
+    y: gsap.getProperty(".pContainer", "y"),
     scale: getScale()
   });
-
-  gsap.to(p, {
-    duration: gsap.utils.random(1, 5),
+  var tl = gsap.timeline();
+  tl.to(p, {
+    duration: gsap.utils.random(0.61, 6),
     physics2D: {
-      velocity: gsap.utils.random(-30, 30),
+      velocity: gsap.utils.random(-23, 23),
       angle: gsap.utils.random(-180, 180),
-      gravity: gsap.utils.random(-5, 40)
+      gravity: gsap.utils.random(-6, 50)
     },
-    rotation: gsap.utils.random(-180, 360),
     scale: 0,
-    onStart: () => flicker(p)
+    rotation: gsap.utils.random(-123, 360),
+    ease: "power1",
+    onStart: flicker,
+    onStartParams: [p],
+    //repeat:-1,
+    onRepeat: (p) => {
+      gsap.set(p, {
+        scale: getScale()
+      });
+    },
+    onRepeatParams: [p]
   });
 
-  particleCount = (particleCount + 1) % numParticles;
+  //
+  //particlePool[particleCount].play();
+  particleCount++;
+  //mainTl.add(tl, i / 1.3)
+  particleCount = particleCount >= numParticles ? 0 : particleCount;
 }
 
 function drawStar() {
-  gsap.timeline({ onUpdate: playParticle })
+  starTl = gsap.timeline({ onUpdate: playParticle });
+  starTl
     .to(".pContainer, .sparkle", {
       duration: 6,
-      motionPath: { path: ".treePath" },
-      ease: "none"
+      motionPath: {
+        path: ".treePath",
+        autoRotate: false
+      },
+      ease: "linear"
     })
     .to(".pContainer, .sparkle", {
       duration: 1,
-      onStart: () => showParticle = false,
+      onStart: function () {
+        showParticle = false;
+      },
       x: treeBottomPath[0].x,
       y: treeBottomPath[0].y
     })
-    .to(".pContainer, .sparkle", {
-      duration: 2,
-      onStart: () => showParticle = true,
-      motionPath: { path: ".treeBottomPath" },
-      ease: "none"
-    });
+    .to(
+      ".pContainer, .sparkle",
+      {
+        duration: 2,
+        onStart: function () {
+          showParticle = true;
+        },
+        motionPath: {
+          path: ".treeBottomPath",
+          autoRotate: false
+        },
+        ease: "linear"
+      },
+      "-=0"
+    )
+    .from(
+      ".treeBottomMask",
+      {
+        duration: 2,
+        drawSVG: "0% 0%",
+        stroke: "#FFF",
+        ease: "linear"
+      },
+      "-=2"
+    );
+
+  //gsap.staggerTo(particlePool, 2, {})
 }
 
 createParticles();
 drawStar();
+//ScrubGSAPTimeline(mainTl)
 
-gsap.globalTimeline.timeScale(1.4);
+mainTl
+  .from([".treePathMask", ".treePotMask"], {
+    duration: 6,
+    drawSVG: "0% 0%",
+    stroke: "#FFF",
+    stagger: {
+      each: 6
+    },
+    duration: gsap.utils.wrap([6, 1, 2]),
+    ease: "linear"
+  })
+  .from(
+    ".treeStar",
+    {
+      duration: 3,
+      //skewY:270,
+      scaleY: 0,
+      scaleX: 0.15,
+      transformOrigin: "50% 50%",
+      ease: "elastic(1,0.5)"
+    },
+    "-=4"
+  )
+
+  .to(
+    ".sparkle",
+    {
+      duration: 3,
+      opacity: 0,
+      ease:
+        "rough({strength: 2, points: 100, template: linear, taper: both, randomize: true, clamp: false})"
+    },
+    "-=0"
+  )
+  .to(
+    ".treeStarOutline",
+    {
+      duration: 1,
+      opacity: 1,
+      ease:
+        "rough({strength: 2, points: 16, template: linear, taper: none, randomize: true, clamp: false})"
+    },
+    "+=1"
+  );
+
+mainTl.add(starTl, 0);
+gsap.globalTimeline.timeScale(1.5);
