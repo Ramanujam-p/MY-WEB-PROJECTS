5e82de85dfc1d4cb5a019d1b558810ac393b037f
Ramanujam-p <pramanujam68@gmail.com>
2026-02-16 18:59:14 +0530

    Code By Rama

--- JSON/LIBRARY MANAGEMENT SYSTEM USING JSON/script.js
+++ JSON/LIBRARY MANAGEMENT SYSTEM USING JSON/script.js
@@ -1,30 +1,30 @@
-function loadjson()
-{
-    let o = `<table border="1">
-        <tr>
-            <th>ID</th>
-            <th>Title</th>
-            <th>Author</th>
-            <th>Genre</th>
-            <th>Availability status</th>
-        </tr>
+function loadjson() {
+    let o = `
+    <table border="1">
+    <tr>
+        <th>ID</th>
+        <th>TITLE</th>
+        <th>AUTHOR</th>
+        <th>GENRE</th>
+        <th>STATUS</th>
+    </tr>
     `;
     fetch("data.json").then(a => a.json()).then(data => {
         let b = data.books;
         b.forEach(c => {
             o += `
-            <tr>
-                <td>${c.id}</td>
-                <td>${c.title}</td>
-                <td>${c.author}</td>
-                <td>${c.genre}</td>
-                <td>${c.status}</td>
-            </tr>`;
+                <tr>
+                    <td>${c.id}</td>
+                    <td>${c.title}</td>
+                    <td>${c.author}</td>
+                    <td>${c.genre}</td>
+                    <td>${c.status}</td>
+                </tr>`;
         });
-             o += `</table>`
-             document.getElementById("output").innerHTML = o 
+        o += `</table >`;
+        document.getElementById("output").innerHTML = o;
     })
-    .catch (e=> {
-        console.log("Error: " + e);
-    });
+        .catch(e => {
+            console.log("error" + e);
+        });
 }
