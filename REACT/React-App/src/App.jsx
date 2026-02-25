3276b13d4ff9d80f772f7153dd18db5668e6e48a
Ramanujam-p <pramanujam68@gmail.com>
2026-02-25 22:32:39 +0530

    Code By Rama

--- REACT/React-App/src/App.jsx
+++ REACT/React-App/src/App.jsx
@@ -1,14 +1,22 @@
-import {useState} from 'react'
+import React, { useState } from 'react'
 const App = () => {
-    const [m, c] = useState("Hello Rama")
-    const f = () => {
-      return(c("Are you Interested to learn React?"))  
+    const [count, setCount] = useState(0)
+    const increase = () => {
+        setCount(a => a + 1)
+    }
+    const decrease = () => {
+        setCount(a=>a-1)
+    }
+    const reset = () => {
+        setCount(0)
     }
     return (
         <div style={{textAlign:"center",marginTop:"50px"}}>
-            <h1>{m}</h1>
-            <button onClick={f}>ClickMe</button>
+            <h1>Count:{count}</h1>
+            <button onClick={increase}>increase</button>
+            <button onClick={decrease}>decrease</button>
+            <button onClick={reset}>reset</button>
         </div>
     )
 }
-export default App
+export default App;
--- REACT/React-App/src/main.jsx
+++ REACT/React-App/src/main.jsx
@@ -1,6 +1,6 @@
+import App from './App.jsx'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { StrictMode } from 'react'
-import App from './App'
 const root = createRoot(document.getElementById("root"))
 root.render(
     <StrictMode>
